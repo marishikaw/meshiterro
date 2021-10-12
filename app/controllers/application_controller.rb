91f0fbe9045a409b388ea43110e0dcc645bdcc80
marishikaw <ishikawamarina@gmail.com>
2021-10-12 06:30:45 +0000

    [Finish] 4-3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
+  # devise利用の機能が使われる場合、その前にconfigure_permitted_parametersアクションを実行
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+  def after_sign_in_path_for(resource)
+    post_images_path
+  end
 
+  # 呼び出された他のコントローラからも参照できる
+  protected
+  
   def configure_permitted_parameters
+    # ユーザ登録（sign_up）の際に、ユーザ名（name）のデータ操作を許可
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '65564d87b529b7479672ddb6ef5759a90b727d0a3a8fd113afe034c9f9f9984a90efd7c3ebd8c3358e1381d4a30b49ce405a06c2653198f7b7c85652d328b5c8'
